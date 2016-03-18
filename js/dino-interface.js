2962771871bd83883fe9663e32fea62b861fdb19
Ben Ronda <bronda95@comcast.net>
2016-03-17 22:16:06 -0700

    fix jquery

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
-  $('#dino').click(function(html){
+  $('#dino').click(function(){
     var paragraphs = $('#paragraphs').val();
     var wordcount = $('#wordcount').val();
+    console.log(paragraphs);
     $('#paragraphs').val("");
     $('#wordcount').val("");
-    $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=' + paragraphs + '&words=' + wordcount)
+    $.get('http://dinoipsum.herokuapp.com/api/',
     {
-      $('#result').html(html);
+      format: 'html',
+      paragraphs: paragraphs,
+      words: wordcount
+    },
+    function(data, _status, _xhr){
+      $('#result').html(data);
     });
   });
 });
